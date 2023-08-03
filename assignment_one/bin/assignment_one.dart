dd1a03bc4ec8281145e7c81c6f1d40151247cc9c
Noushed Ahomed Jholok <80415299+na-jholok@users.noreply.github.com>
2023-08-04 00:12:20 +0600

    Add files via upload

---
+++ assignment_one/bin/assignment_one.dart
@@ -0,0 +1,11 @@
+void main() {
+  int a = 7;
+  int b = 3;
+  print("a = $a , b = $b");
+  print("Addition $a + $b = ${a+b}");
+  print("Subtraction $a - $b = ${a-b}");
+  print("Multipication $a * $b = ${a*b}");
+  print("Divicion $a / $b = ${a/b}");
+  print("Modulus $a % $b = ${a%b}");
+
+}
